68aa3945919c000238d53eb953b8fb4ee496e122
johni1228 <akirus1228@gmail.com>
2021-12-10 22:42:16 +0900

    feat: Create erc20 token.

---
+++ contracts/Token.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+
+contract QKCToken is ERC20, Ownable {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+ 
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+ 
+    constructor() public {
+        symbol = "QKC";
+        name = "QuikNode Coin";
+        decimals = 2;
+        _totalSupply = 100000;
+        balances[YOUR_METAMASK_WALLET_ADDRESS] = _totalSupply;
+        emit Transfer(address(0), YOUR_METAMASK_WALLET_ADDRESS, _totalSupply);
+    }
+}
