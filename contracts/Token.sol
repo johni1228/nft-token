53a7bd8dd5d9a9822915f7c4d011d65eb50376e9
johni1228 <akirus1228@gmail.com>
2021-12-15 22:43:18 +0900

    chore: Update simple token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,21 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; 
 
-contract Token is ERC20, Ownable {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
- 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
- 
-    constructor() public {
-        symbol = "UNIV";
-        name = "UniverseToken";
-        decimals = 18;
-        _totalSupply = 100000000;
-        balances[owner()] = _totalSupply;
-        emit Transfer(address(0), owner(), _totalSupply);
+contract Token is is ERC20, Ownable {
+    constructor() ERC20("UniverseToken", "UNIV") {
+        _mint(owner(), 100000000);
     }
 }
