d6bbb1842a12f62b31f6e5a4a88baf3cf3e7b264
johni1228 <akirus1228@gmail.com>
2021-12-16 22:58:16 +0900

    chore: Update migrate.json

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,12 +2,17 @@ const Universe = artifacts.require('UniVerse');
 const UniverseToken = artifacts.require('Token');
 const baseURI = 'https://ras-nft.herokuapp.com/api/v0/nfts/'; //TODO: update api
 
-module.exports = function(deployer) {
-  deployer.deploy(UniverseToken).then(() => {
-    console.log('UniVerseToken is deployed.');
-  });
+// module.exports = function (deployer) {
+//   deployer.deploy(UniverseToken).then(() => {
+//     console.log('UniVerseToken is deployed.');
+//   });
 
-  deployer.deploy(Universe, baseURI, UniverseToken.address).then(() => {
-    console.log('UniVerse is deployed.');
-  });
-};
+//   deployer.deploy(Universe, baseURI, UniverseToken.address).then(() => {
+//     console.log('UniVerse is deployed.');
+//   });
+// };
+
+module.exports = async function (deployer) {
+  const token = await UniverseToken.deployed();
+  await Universe.deployed(baseURI, token.address);
+}; 
