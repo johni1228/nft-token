b88f7ca992a66d4888037e0fe704c95a588532d3
johni1228 <akirus1228@gmail.com>
2021-12-17 00:14:48 +0900

    fix: Add token real address

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -13,9 +13,12 @@ const baseURI = 'https://ras-nft.herokuapp.com/api/v0/nfts/'; //TODO: update api
 // };
 
 module.exports = async function (deployer) {
-    const token = deployer.deploy(UniverseToken).then(() => {
-      console.log('UniVerseToken is deployed.');
-    });
-    console.log(token.address)
-  await Universe.deployed(baseURI, token.address, {deployer});
-}; 
+  const token = await deployer.deploy(UniverseToken).then(() => {
+    console.log('UniVerseToken is deployed.');
+  });
+  console.log(token.address)
+  deployer.deploy(Universe, baseURI, "0x3F8c2A5dAC285f56555bac2E91d47D51c356B417").then(() => {
+    console.log('UniVerse is deployed.');
+  });
+};
+};
