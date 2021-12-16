8aae0794e41e64335fc1e951279370872ef4b1b5
johni1228 <akirus1228@gmail.com>
2021-12-17 00:24:10 +0900

    chore: Remove token deploy script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -13,10 +13,9 @@ const baseURI = 'https://ras-nft.herokuapp.com/api/v0/nfts/'; //TODO: update api
 // };
 
 module.exports = async function (deployer) {
-  const token = await deployer.deploy(UniverseToken).then(() => {
-    console.log('UniVerseToken is deployed.', UniverseToken.address);
-  });
-  console.log(token.address)
+  // deployer.deploy(UniverseToken).then(() => {
+  //   console.log('UniVerseToken is deployed.', UniverseToken.address);
+  // });
   deployer.deploy(Universe, baseURI, "0x3F8c2A5dAC285f56555bac2E91d47D51c356B417").then(() => {
     console.log('UniVerse is deployed.');
   });
